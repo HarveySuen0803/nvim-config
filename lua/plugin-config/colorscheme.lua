c1b49eecf98c1766d5e8f8fa2b3aa627a4f25ea4
Harvey Suen <harveysuen0803@gmail.com>
2023-12-01 09:54:41 +0800

    perf(global): colorscheme

--- lua/plugin-config/colorscheme.lua
+++ lua/plugin-config/colorscheme.lua
@@ -1,7 +1,6 @@
 vim.o.termguicolors = true
 
 -- github
-require('github-theme').setup({
-})
+require('github-theme').setup()
 
-vim.cmd('colorscheme github_dark_high_contrast')
+vim.cmd('colorscheme github_light_high_contrast')
